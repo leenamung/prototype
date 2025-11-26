e889796a10cda670795f3f7f45ff13773374c4ab
이남웅 <papapia@naver.com>
2025-11-26 21:11:04 +0900

    vercel 빌드 오류 해결

--- app/agit/page.tsx
+++ app/agit/page.tsx
@@ -19,11 +19,10 @@ async function getMyAgits() {
 
 export default async function AgitListPage() {
   const myAgits = await getMyAgits();
-  const userProfileImage = "https://readdy.ai/api/search-image?query=portrait%20of%20a%20young%20asian%20woman%2C%20soft%20lighting%2C%20warm%20tones%2C%20natural%20look%2C%20gentle%20smile%2C%20high%20quality%2C%20professional%20photo&width=100&height=100&seq=1&orientation=squarish";
   return (
     <>
       {/* 1. 헤더 영역 */}
-      <AgitMainNavigationBar userProfileImage={userProfileImage} />
+      <AgitMainNavigationBar />
 
       {/* 2. 콘텐츠 영역 (pt-14) */}
       <div className="pt-14">
